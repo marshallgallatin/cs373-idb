25359b48e48fcb58c45e7f38d54a6a9972260266
Marshall Gallatin <marshallgallatin@gmail.com>
2016-03-22 20:40:18 -0500

    a simple simple simple static page to deliver

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hellu Vurld. Zees is zee-a vebsite-a fur CS373 IDB Pruject. Bork Bork Bork!"
+def splash():
+    return app.send_static_file('html/splash.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
