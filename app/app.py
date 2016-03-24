a722b8f5436f86452cc97bece5612bd6c6abe142
Marshall Gallatin <marshallgallatin@gmail.com>
2016-03-23 21:45:02 -0500

    added bootstrap and two static webpages that are empty, updated app.py to serve any webpage from the static/html folder

--- app/app.py
+++ app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def splash():
     return app.send_static_file('html/splash.html')
 
+@app.route("/<path:path>")
+def static_html(path):
+    return app.send_static_file('html/{}'.format(path))
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
