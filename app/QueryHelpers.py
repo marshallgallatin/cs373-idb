24750a7d57813b1adfee14bdce20a947f61beb2f
Joshie196 <joshdcannon@gmail.com>
2016-04-07 21:43:15 -0500

    Update QueryHelpers.py

--- app/QueryHelpers.py
+++ app/QueryHelpers.py
@@ -16,10 +16,6 @@ def ensureDictExactlyContains(dictionary, *args):
     if len(set(dictionary.keys()) ^ set(args)) != 0:
         raise BadQueryException()
 
-def ensureTypeIs(arg, typeArg):
-    if not type(arg) == typeArg:
-        raise BadQueryException()
-
 def ensureListIsntEmpty(aList):
     if len(aList) == 0:
         raise BadQueryException()
