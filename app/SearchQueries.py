bfea7a72590e23be47f5e401ef21a18f9be187ae
K.C. Hawes-Domingue <HawesDomingue@utexas.edu>
2016-04-21 02:37:14 -0500

    Added search support for dietary restriction and cuisine filtering (no snippets yet)

--- app/SearchQueries.py
+++ app/SearchQueries.py
@@ -14,34 +14,71 @@ numberOfWordsOnEitherSideOfMatch = 3
 
 def search(keywords):
     with sessionInstance() as session:
+
         # Diet and cuisine restrictions
-        knownDietRestrictions = ['vegetarian', 'vegan', 'gluten_free', 'dairy_free']
-        glutenFree = ['gluten-free']
-        dairyFree = ['dairy-free', 'lactose-free', 'nondairy', 'non-dairy']
-        restrictions = {}
+        parsedKeywords = list(keywords)
+        filteredRecipesAnd = session.query(models.Recipe)
+        filteredRecipesOr = None
         for keyword in keywords:
             kw = keyword.lower()
-            if kw in knownDietRestrictions:
-                restrictions[kw] = True
-                keywords.remove(kw)
-            if kw in glutenFree:
-                restrictions['gluten_free'] = True
-                keywords.remove(kw)
-            if kw in dairyFree:
-                restrictions['dairy_free'] = True
-                keywords.remove(kw)
-            if kw in Cuisine.__members__:
-                restrictions['cuisine'] = Cuisine[kw].name
-                keywords.remove(kw)
-
-        filteredRecipes = session.query(models.Recipe).filter_by(**restrictions)
+            # Conditional from hell... dear Mr. Downing, please show me how to refactor in Python
+            if kw in ['vegetarian', 'meat-free', 'meat free']:
+                filteredRecipesAnd = filteredRecipesAnd.filter(models.Recipe.vegetarian)
+                if filteredRecipesOr:
+                    filteredRecipesOr = filteredRecipesOr.union(session.query(models.Recipe).filter(models.Recipe.vegetarian))
+                else:
+                    filteredRecipesOr = session.query(models.Recipe).filter(models.Recipe.vegetarian)
+                parsedKeywords.remove(kw)
+            elif kw in ['vegan']:
+                filteredRecipesAnd = filteredRecipesAnd.filter(models.Recipe.vegan)
+                if filteredRecipesOr:
+                    filteredRecipesOr = filteredRecipesOr.union(session.query(models.Recipe).filter(models.Recipe.vegan))
+                else:
+                    filteredRecipesOr = session.query(models.Recipe).filter(models.Recipe.vegan)
+                parsedKeywords.remove(kw)
+            elif kw in ['gluten-free', 'gluten free']:
+                filteredRecipesAnd = filteredRecipesAnd.filter(models.Recipe.gluten_free)
+                if filteredRecipesOr:
+                    filteredRecipesOr = filteredRecipesOr.union(session.query(models.Recipe).filter(models.Recipe.gluten_free))
+                else:
+                    filteredRecipesOr = session.query(models.Recipe).filter(models.Recipe.gluten_free)
+                parsedKeywords.remove(kw)
+            elif kw in ['dairy-free', 'lactose-free', 'nondairy', 'non-dairy', 'dairy free']:
+                filteredRecipesAnd = filteredRecipesAnd.filter(models.Recipe.dairy_free)
+                if filteredRecipesOr:
+                    filteredRecipesOr = filteredRecipesOr.union(session.query(models.Recipe).filter(models.Recipe.dairy_free))
+                else:
+                    filteredRecipesOr = session.query(models.Recipe).filter(models.Recipe.dairy_free)
+                parsedKeywords.remove(kw)
+            elif kw in Cuisine.__members__:
+                filteredRecipesAnd = filteredRecipesAnd.filter(models.Recipe.cuisine == Cuisine[kw].name)
+                if filteredRecipesOr:
+                    filteredRecipesOr = filteredRecipesOr.union(session.query(models.Recipe).filter(models.Recipe.cuisine == Cuisine[kw].name))
+                else:
+                    filteredRecipesOr = session.query(models.Recipe).filter(models.Recipe.cuisine == Cuisine[kw].name)
+                parsedKeywords.remove(kw)
 
         def ilikeGenerator(column):
             return map(lambda x:column.ilike('%' + x + '%'), keywords)
 
-        recipesAnd = filteredRecipes.filter(or_(and_(*ilikeGenerator(models.Recipe.title)), and_(*ilikeGenerator(models.Recipe.instructions)))).all()
+        def ilikeGeneratorParsed(column):
+            return map(lambda x: column.ilike('%' + x + '%'), parsedKeywords)
+
+        # If we still have keywords beyond the special keywords for dietary restrictions and cuisine, grep them
+        if (parsedKeywords):
+            filteredRecipesAnd = filteredRecipesAnd.filter(or_(and_(*ilikeGeneratorParsed(models.Recipe.title)), and_(*ilikeGeneratorParsed(models.Recipe.instructions))))
+        # But still do a lookup on the special keywords within titles and instructions
+        keywordAnd = session.query(models.Recipe).filter(or_(and_(*ilikeGenerator(models.Recipe.title)), and_(*ilikeGenerator(models.Recipe.instructions))))
+        recipesAnd = filteredRecipesAnd.union(keywordAnd).all()
+
         ingredientsAnd = session.query(models.Ingredient).filter(or_(and_(*ilikeGenerator(models.Ingredient.name)), and_(*ilikeGenerator(models.Ingredient.scientific_name)))).all()
-        recipesOr = filteredRecipes.filter(or_(or_(*ilikeGenerator(models.Recipe.title)), or_(*ilikeGenerator(models.Recipe.instructions)))).all()
+
+        if (parsedKeywords):
+            filteredRecipesOr = filteredRecipesAnd.filter(or_(or_(*ilikeGeneratorParsed(models.Recipe.title)),
+                                                              or_(*ilikeGeneratorParsed(models.Recipe.instructions))))
+        keywordOr = session.query(models.Recipe).filter(or_(or_(*ilikeGenerator(models.Recipe.title)), or_(*ilikeGenerator(models.Recipe.instructions))))
+        recipesOr = [recipe for recipe in filteredRecipesOr.union(keywordOr).all() if recipe not in recipesAnd]
+
         ingredientsOr = session.query(models.Ingredient).filter(or_(or_(*ilikeGenerator(models.Ingredient.name)), or_(*ilikeGenerator(models.Ingredient.scientific_name)))).all()
 
         def getAllSnippets(iterable):
